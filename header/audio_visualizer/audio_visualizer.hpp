221e580e2585695743ead09c47d9deda166729d3
= <=>
2022-04-08 17:07:56 +0200

    commit

--- header/audio_visualizer/audio_visualizer.hpp
+++ header/audio_visualizer/audio_visualizer.hpp
@@ -4,9 +4,8 @@
 #include "../opengl_gui/opengl_gui.hpp"
 #include "draw_functions.hpp"
 #include "audio_input.hpp"
-
 #include <glm/gtx/color_space.hpp>
-#include <BTrack.h>
+#include <fftw3.h>
 
 namespace audio_visualizer
 {
@@ -24,11 +23,10 @@ namespace audio_visualizer
         void setImage(int _newImageIndex);
 
     private:
-        static const double COLOR_TRANSITION_DURATION;
+        static const double COLOR_TRANSITION_DURATION, IMAGE_TRANSITION_DURATION;
 
         bool *stop = nullptr;
         AudioInput *input = nullptr;
-        BTrack *beatTrack = nullptr;
 
         std::vector<unsigned char> waveformTextureBytes;
         int waveformTextureWidth, waveformTextureHeight;
@@ -36,11 +34,18 @@ namespace audio_visualizer
         glm::vec3 HSV = glm::vec3(0.0f, 0.0f, 1.0f), prevHSV;
         double colorTransitionStartTime = -100.0;
 
-        float prevAudioPeak = 0.0f;
+        std::vector<double> prevAudioPeak;
+        double prevAudioPeakOutput = 0.0;
 
         opengl_gui::Style::COLOR *bgOverlayColor = nullptr;
 
         int imageIndex = 0;
+        double imageTransitionStartTime = -100.0;
+        bool isImageTransitionNewImageSet = false;
+
+        std::vector<double> frequencyDomain;
+        fftw_complex *frequencyComplex = nullptr;
+        fftw_plan frequencyPlan;
 
         void logData();
     };
--- src/audio_visualizer/audio_visualizer.cpp
+++ src/audio_visualizer/audio_visualizer.cpp
@@ -1,13 +1,15 @@
 #include "../../header/audio_visualizer/audio_visualizer.hpp"
 
-const double audio_visualizer::App::COLOR_TRANSITION_DURATION = 4.0;
+const double audio_visualizer::App::COLOR_TRANSITION_DURATION = 4.0,
+             audio_visualizer::App::IMAGE_TRANSITION_DURATION = 1.0;
 
 audio_visualizer::App::App(bool *stop, std::string cssCode, std::vector<std::string> bgImageFilePaths, opengl_gui::Window *window)
     : opengl_gui::DrawLoop(window)
 {
     this->stop = stop;
     input = new AudioInput();
-    beatTrack = new BTrack(input->BUFFER_SIZE / 2, input->BUFFER_SIZE);
+
+    prevAudioPeak.resize(60, 0.0);
 
     std::cout << "Loading image data\n";
 
@@ -47,12 +49,16 @@ audio_visualizer::App::App(bool *stop, std::string cssCode, std::vector<std::str
 
     imageElements.push_back(new opengl_gui::ImageElement(textures.back(), "waveform"));
     guiRenderer->addElement(imageElements[1]);
+
+    frequencyComplex = (fftw_complex *)calloc(input->BUFFER_SIZE / 2 + 1, sizeof(fftw_complex));
+    frequencyDomain.resize(input->BUFFER_SIZE / 4);
 }
 
 void audio_visualizer::App::terminate()
 {
+    fftw_free(frequencyComplex);
+
     delete input;
-    delete beatTrack;
 
     if (guiRenderer)
         delete guiRenderer;
@@ -94,7 +100,6 @@ void audio_visualizer::App::loop()
 
         glm::vec3 currentHSV = HSV;
         double colorTransitionCurrentTime = window->getCurrentTime() - colorTransitionStartTime;
-
         if (colorTransitionCurrentTime < COLOR_TRANSITION_DURATION)
             currentHSV = glm::mix(prevHSV, HSV, colorTransitionCurrentTime / COLOR_TRANSITION_DURATION);
 
@@ -103,15 +108,73 @@ void audio_visualizer::App::loop()
         audio_visualizer::drawWaveform(waveformTextureBytes, waveformTextureWidth, waveformTextureHeight, color, input->bufferData);
         textures.back()->update(waveformTextureBytes.data(), waveformTextureWidth, waveformTextureHeight, GL_RGBA);
 
-        beatTrack->processAudioFrame(input->bufferData.data());
+        double imageTransitionCurrentTime = window->getCurrentTime() - imageTransitionStartTime;
+        if (imageTransitionCurrentTime < IMAGE_TRANSITION_DURATION)
+        {
+            if (imageTransitionCurrentTime < IMAGE_TRANSITION_DURATION / 2.0)
+            {
+                bgOverlayColor->value.a = (imageTransitionCurrentTime / (IMAGE_TRANSITION_DURATION / 2.0)) * 0.7 + 0.3;
+            }
+            else
+            {
+                if (!isImageTransitionNewImageSet)
+                {
+                    imageElements[0]->setImage(textures[imageIndex]);
+                    isImageTransitionNewImageSet = true;
+                }
+                bgOverlayColor->value.a = 1.0 - ((imageTransitionCurrentTime - (IMAGE_TRANSITION_DURATION / 2.0)) / (IMAGE_TRANSITION_DURATION / 2.0)) * 0.7;
+            }
+        }
+        else
+        {
+            double audioPeak = 0.0;
+            if (window->isLmbDown())
+            {
+                for (int i = 0; i < input->BUFFER_SIZE; i++)
+                {
+                    double _abs = abs(input->bufferData[i]);
+                    if (_abs > audioPeak)
+                        audioPeak = _abs;
+                }
+            }
+            else
+            {
+                frequencyPlan = fftw_plan_dft_r2c_1d(input->BUFFER_SIZE, input->bufferData.data(), frequencyComplex, FFTW_ESTIMATE);
+                fftw_execute(frequencyPlan);
 
-        float audioPeak = beatTrack->beatDueInCurrentFrame();
-        if (audioPeak - prevAudioPeak < -0.1f)
-            audioPeak = prevAudioPeak - 0.1f;
+                for (size_t i = 0; i < frequencyDomain.size(); i++)
+                    frequencyDomain[i] = 20.0 * log10(sqrt(pow(frequencyComplex[i + 1][0], 2) + pow(frequencyComplex[i + 1][1], 2)));
 
-        prevAudioPeak = audioPeak;
+                fftw_destroy_plan(frequencyPlan);
 
-        bgOverlayColor->value.a = (1.0f - audioPeak * 0.3f) - 0.7f;
+                for (int i = 0; i < 10; i++)
+                    if (frequencyDomain[i] > audioPeak)
+                        audioPeak = frequencyDomain[i];
+
+                audioPeak /= 50.0;
+            }
+
+            double prevAudioPeakAverage = 0.0;
+            for (int i = 0; i < prevAudioPeak.size(); i++)
+                prevAudioPeakAverage += prevAudioPeak[i];
+
+            prevAudioPeakAverage /= prevAudioPeak.size();
+
+            double audioPeakOutput = (audioPeak - prevAudioPeakAverage) / (1.0 - prevAudioPeakAverage);
+            if (audioPeakOutput > 1.0)
+                audioPeakOutput = 1.0;
+            else if (audioPeakOutput < 0.0)
+                audioPeakOutput = 0.0;
+
+            if (audioPeakOutput - prevAudioPeakOutput < -0.1)
+                audioPeakOutput = prevAudioPeakOutput - 0.1;
+
+            bgOverlayColor->value.a = (1.0 - audioPeakOutput * 0.3) - 0.7;
+
+            prevAudioPeak.erase(prevAudioPeak.begin());
+            prevAudioPeak.push_back(audioPeak);
+        }
+
         elements[0]->styleChange = true;
 
         guiRenderer->update();
@@ -138,9 +201,10 @@ void audio_visualizer::App::setMultiplier(float _newMultiplier)
 }
 void audio_visualizer::App::setImage(int _newImageIndex)
 {
-    if (_newImageIndex >= 0 && _newImageIndex < textures.size() - 1)
+    if (_newImageIndex >= 0 && _newImageIndex < textures.size() - 1 && _newImageIndex != imageIndex)
     {
-        imageElements[0]->setImage(textures[_newImageIndex]);
+        isImageTransitionNewImageSet = false;
+        imageTransitionStartTime = window->getCurrentTime();
         imageIndex = _newImageIndex;
         logData();
     }
